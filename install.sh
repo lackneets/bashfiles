62c504bea2339aebac0575723b8e1fe8d4b333b1
lackneets <lackneets@gmail.com>
2016-01-15 11:01:43 +0800

    use script path

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-ln -s $(readlink -f .)/.bashrc       ~/.bashrc
-ln -s $(readlink -f .)/.bash_profile ~/.bash_profile
-ln -s $(readlink -f .)/.gitconfig    ~/.gitconfig
-ln -s $(readlink -f .)/.tmux.conf    ~/.tmux.conf
-ln -s $(readlink -f .)/.tmux         ~/.tmux
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=`dirname $SCRIPT`
+
+ln -s $SCRIPTPATH/.bashrc       ~/.bashrc
+ln -s $SCRIPTPATH/.bash_profile ~/.bash_profile
+ln -s $SCRIPTPATH/.gitconfig    ~/.gitconfig
+ln -s $SCRIPTPATH/.tmux.conf    ~/.tmux.conf
+ln -s $SCRIPTPATH/.tmux         ~/.tmux
 
 # git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 # vim +BundleInstall +qall
